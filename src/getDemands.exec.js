1ff98639dc6a39a1c5751f4ebc7ece4639bc6675
lomaxap <andrewlomaxr@gmail.com>
2020-05-13 23:09:44 -0400

    add demands

--- src/getDemands.exec.js
+++ src/getDemands.exec.js
@@ -1,6 +1,189 @@
-const fetch = require("isomorphic-fetch");
+// const fetch = require("isomorphic-fetch");
 
 module.exports = async () => {
-  const demands = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return demands.json();
+  //const demands = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //return demands.json();
+
+  return [
+    {
+      category: "Federal Government",
+      lead:
+        "All states are receiving a minimum of $1.25 billion from the federal government. DC is receiving much less, when our population is more than some states. The Federal Government does not acknowledge our full humanity, even when they are in our backyard. We need the full monetary support of the Federal Government.",
+      list: [
+        "The Federal Government will treat DC like a state and will give DC COVID-19 crisis funds. DC is treated like a state in all other federal funding decisions including transportation, education, etc. and should be in this crisis situation.",
+        "The Federal Government must honor obligations to Indigenous communities by providing necessary medical personnel when and where needed and requested to combat this crisis.",
+        "The Federal Government will use every possible means to rapidly expand production of critical medical supplies",
+        "The Federal Government will expand direct cash assistance and expand it to all households ",
+        "The Federal Government will expand unemployment insurance and paid leave benefits",
+        "The Federal Government will eliminate emergency relief limits based on immigration status or type of employment",
+        "The Federal Government will increase SNAP and other critical safety-net benefits",
+      ],
+    },
+    {
+      category: "Policing",
+      lead:
+        "The Metropolitan Police Department has a history of mistreating, profiling and arresting Black, Brown and poor people in DC. We recognize that some residents find safety in the police and therefore we need the police to follow protocol that will keep all of us safe.",
+      list: [
+        "MPD will follow the same guideline of social distancing. If patrolling is needed, they will remain 6 feet away from others, and wear appropriate PPEs",
+        "MPD will halt arrests of people for buying and selling narcotics, as this practice disproportionately impacts people with addictions and places them at risk.",
+        "MPD & the DC Government will demand the US Parole Commission eliminate parole and probation revocations.",
+        "MPD will refrain from arresting all those who don't pose a credible and articulated public safety risk- and if arrests are made MPD must write out the safety risk and information must be publicly accessible.",
+        "MPD officers will undergo mental health screenings, trauma informed care trainings and therapy  ",
+        "MPD will be repurposed during this period to other much-needed public servant work such as support of DOES to get people’s UI payments out.",
+        "MPD will in no way cooperate with ICE",
+      ],
+    },
+    {
+      category: "Incarceration",
+      lead:
+        "The DC Jail is unsafe, unsanitary, and was in no condition to house people before COVID 19. Now, as a space where DC residents are unable to follow social distancing, it’s dangerous and unjust to keep anyone inside the jail.",
+      list: [
+        "Department of Corrections will immediately release all people incarcerated in the DC Jail – particularly those over 50, trans or that the Center for Disease Control (CDC) has identified as vulnerable ",
+        "Ensure that all people who continue to be incarcerated have access to PPE and products to ensure a sanitary environment at no cost to them.",
+        "Ensure that COVID-19 testing, prevention, care, and treatment services are available at no cost to those who are incarcerated or detained.",
+        "Immediately release all people incarcerated at DC jail who are within six months of their release date",
+        "MPD will not release anyone to Immigration and Customs Enforcement (ICE)",
+        "Department of Corrections will release all current DC Jail and CTF residents who were arrested for misdemeanors",
+        "Department of Corrections will postpone revocation hearings and release all DC residents currently held on technical violations",
+        "Department of Corrections will provide compassionate release and release all elders who are medically fragile (demand of the US Bureau of Prisons)",
+        "Upon release, the DC Government will provide the formerly incarcerated with resources like food, water, access to testing and shelter. Housing is always a top concern and in this pandemic it’s even more important to ensure people have safe, stable places for shelter in place and socially distance. ",
+        "The DC Government will remove any conviction-based restrictions for government benefits",
+        "The DC Government will eliminate any and all costs associated with probation or parole",
+        "The DC Jail will ensure that there is transparency regarding data on contraction of and deaths associated with COVID-19",
+        "The Department of Youth Services will decarcerate all young people",
+        "The DC Government will immediately decriminalize and stop arrests related to sex work and release anyone in D.C. custody charged with prostitution-related arrests.",
+      ],
+    },
+    {
+      category: "Homelessness",
+      lead:
+        "Before COVID- 19, 6,521 DC Residents experienced homelessness every night. We believe housing is a human right and required for anyone attempting to practice social distancing. The DC Government needs to prioritize our most impacted communities first and recognize housing is healthcare.",
+      list: [
+        "DC Government will provide portable bathrooms and access to public restrooms for all homeless residents  ",
+        "Department of Human Services will place in-house and maintain all applicants who self-certify as homeless",
+        "The Department of Human Services will not terminate anyone from shelter during the public health emergency",
+        "The Department of Human Services will not terminate anyone from the rapid rehousing program for the duration of the public health emergency",
+        "The DC Government will incentivize hotels and other empty dwellings to house the DC homeless population.",
+      ],
+    },
+    {
+      category: "Housing",
+      lead:
+        "Thousands of people have lost the ability to pay their rent or mortgage due to loss of income. While there is a moratorium on late fees and rent increases during the emergency, many are accruing debt in the form of back rent and missed mortgage payments. The impact of this virus will stay with us for years, and our residents deserve to have a home that they don’t have to worry about losing.",
+      list: [
+        "Mortgage lenders and all landlords including the District of Columbia Housing Authority (DCHA) will cancel rent and mortgage payments for the duration of the state of emergency, plus one month ",
+        "DC Government will enforce a two-year rent freeze on rent increases while the city recovers. ",
+        "DC Government will provide the right to counsel for all DC residents in eviction cases. ",
+        "The DC Government will triple funding and vastly expand who is eligible for the Emergency Rental Assistance Program. They will also eliminate the annual limits on how much you can get and how often you can get it. ",
+        "The DC Council will enact into law the Reclaim Rent Control platform in its entirety. ",
+        "DCHA will provide continued maintenance, especially health and safety repairs. And provide a guarantee of habitability.",
+        "DCHA and private landlords will make sure maintenance folks have appropriate Personal Protective Equipment (PPEs) before going into resident's homes ",
+        'DC Council will press "pause" on all redevelopment projects until resident engagement processes can also resume',
+        "DC Government will ensure that all undocumented folks/folks not on leases will have the same protection from eviction during COVID-19 as any other tenant",
+        "The DCRA will act as a mediator--not an enforcer in the case of neighbor complaints over other neighbors for noise, drug use, trash and other petty nuisance complaints. These are often unjustly enforced over Black and Brown sex workers who are forced to be in vulnerable relationships with landlords as a result.",
+      ],
+    },
+    {
+      category: "Work/Economic Justice",
+      lead:
+        "DC residents deserve economic justice, regardless of the way they receive money. Economic justice includes supporting each resident holistically- our families, our communities and our small businesses.",
+      list: [
+        "All businesses operating in DC will provide a minimum of 8 weeks of paid leave for everyone",
+        "DC Government will expand the paid sick law to allow anyone to take up to 10 emergency paid sick days without any waiting period for coronavirus-related testing, treatment, recovery and caregiving. ",
+        "DC Government will provide an expansion of Unemployment Insurance and include sex workers, undocumented folks, gig workers and all those in need",
+        "DC Government will expand the Family Medical Leave Act to cover public health emergencies including coronavirus, reduce employer threshold and tenure requirements for this type of FMLA, and make the FMLA retroactively begin on March 1st to provide coverage for those who need(ed) family and medical leave. ",
+        "DC Government will extend anti-poverty programs",
+        "DC Government will provide supports to Small Businesses- particularly run by Black people and in Wards 7 & 8",
+        "All employers of sanitation workers, grocery store employees, childcare workers and janitorial staff will provide appropriate protective gear and hazard pay",
+      ],
+    },
+    {
+      category: "Health",
+      lead:
+        "COVID-19 is a public health emergency that is impacting all other parts of our DC healthcare network. All residents need to have access to comprehensive medical attention and to be physically safe and as healthy as possible. The DC Government must protect our essential workers and prioritize all those most vulnerable to the virus.",
+      list: [
+        "Hospitals will ensure folks giving birth can have the support team they choose and the DC Government will provide increased funding for birthing centers and midwives who do home births. ",
+        "DC Government will ensure healthcare facilities will have all the medical equipment, ventilators, PPE, etc that they need",
+        "DC Government will increase funding and housing for Domestic Violence organizations",
+        "DC Government will financially support mental health hotlines and provide year long therapy vouchers for whoever needs them",
+        "DCPS and the DC Charter board will ensure that all schools provide therapists to actively reach out to schools and young people. Once students return to schools, the DC Government will provide money for mental health support in all schools ",
+        "The DC Government will increase access to COVID-19 testing (particularly in Wards 7 and 8). DC Government will ensure that access to testing is equitable and accessible (including language accessibility). Distribute testing kits to community based orgs that provide medical services like; Bread for the City, Mary’s Center and Community of Hope to administer testing.",
+        "ICE and MPD will be kept out of all hospitals",
+        "DC Government will ensure the continuation of essential services such as abortion access and contraceptive access",
+        "DC Government will ensure that people who need non COVID-19 related medical support will be able to access it without being exposed to the virus ",
+        "DC Government will ensure that DC residents will still be able to access non- COVID related medicines",
+        "DC Government will ease the process to access Methadone to reduce long lines where social distancing has become a problem. Allow people to have multiple doses to reduce visits.",
+        "DC Government will ensure that telehealth/telemedicine options be made available to all who need it, with a particular focus on Black people, trans folks, and communities with low incomes ",
+        "DC Government and Children’s Hospital will protect school nurses from lay-offs due to school closures.",
+      ],
+    },
+    {
+      category: "Education",
+      lead:
+        "COVID 19 has made the educational inequities that existed in DC before the virus, much more evident now. Students all over the city deserve a quality education, rooted in equity and racial justice. DC students, families and school staff need to feel confident that they will have the information, support, equipment and flexibility needed during the pandemic.",
+      list: [
+        "DC Public Schools (DCPS) and the DC Charter board will provide all students in DC schools with tablets, internet access and/or wifi hotspots, whether they have a social security number or not.  ",
+        "DCPS & DC Charter Schools will not refer students to CFSA (Child and Family Services Agency) for not attending online courses ",
+        "DCPS and the DC Charter board will train all teachers and staff in online teaching through a racial justice lens",
+        "DC Government will put pressure on the federal government to cancel all student loans ",
+        "DCPS and the DC Charter board will suspend teacher impact evaluations for the year, without a negative impact on their salaries",
+        "DCPS and the DC Charter board will suspend mandatory summer school and leave the decision to students and families ",
+        "All seniors on track for graduation at the time of March 16th, 2020 will graduate",
+        "All students in 9th, 10th or 11th grade will not be held back or made to go to summer school based on their grades after March 16th, 2020.",
+        "All material taught by DCPS faculty via online lecture will also be shared with students through written email.",
+        "Fully fund and ensure mental health counselors to support student transitions into the new school year.",
+        "DCPS will continue to pay all non salaried staff (aides, maintenance, food service, admin assistants, etc)",
+        "All schools in DC should be thoroughly cleaned and inspected before students return to school ,and thorough cleanings and inspections should be done every weekend until the end of the summer (August) to prevent the lingering of the virus.",
+      ],
+    },
+    {
+      category: "Children/Young People",
+      lead:
+        "Children and young people need to be prioritized the most in this city. By centering our youngest residents, their needs, their unique differences, their caretakers and families and the systems they depend on we will be able to learn how to best support everyone through this virus.",
+      list: [
+        "DCPS and DC Charter schools and DPR playgrounds will be continually deep cleaned when they reopen ",
+        "DC Government will make sure that young people know where to get soap, sanitizer and supplies to stay safe",
+        "DC Public Libraries will send free books every month to all children regardless of age",
+        "DC Foster Care System will provide tablets and internet access to young people in foster care to speak to their families ",
+        "Provide grants to private childcare centers so they can re-open",
+        "DC Government will pass the Childcare Stabilization Package for DC childcare providers ($20M for March-June to cover operating expenses, waived co-pays, hazard pay, full cost of subsidy payment levels)",
+        "DC Government will maintain current investments in childcare subsidy programs and advocate for additional $10M to cover anticipated COVID-19 related expenses ",
+        "DC Government will maintain investments in Healthy Start, Healthy Futures, Help Me Grow, and existing Home Visiting programs.",
+      ],
+    },
+    {
+      category: "Disability Rights",
+      lead:
+        "DC’s residents with disabilities deserve additional support during this pandemic. Many services, medical facilities and educational spaces are closed or overwhelmed with need. The DC Government needs to make sure those of us with disabilities have what we need.",
+      list: [
+        "DC Government will expand DC Home and Community Based Services waiver eligibility and coverage",
+        "DC Government will fund the (Direct Support Professional) DSP Wage Act ",
+        "DC Government will support people with disabilities by creating and expanding virtual access to talk therapy for all who may need it during this time.",
+        "DC Government will address transportation assistance and accessible care for people with disabilities ",
+        "DC Government will free provide interpretation services for deaf residents",
+      ],
+    },
+    {
+      category: "Anti-Discrimination",
+      lead:
+        "Discrimination and injustice have no place in our city. During the COVID-19 pandemic the DC Government needs to ensure all of our residents, particularly those facing additional injustice because of the pandemic, will be protected and supported.",
+      list: [
+        "DC Government will ensure all Asian residents are safe and that anti-Asian racism won't be tolerated",
+        "DC Government will ensure all homeless residents are not ticketed or arrested ",
+        "DC Government will ensure all marginalized people (Black people, trans people, people with disabilities, etc) are not discriminated against in triage ",
+        "DC Government will ensure that all communication about COVID-19 will be available in all languages spoken in DC ",
+        "DC Government will acknowledge the impact of white supremacy, anti-Blackness and capitalism on DC residents and actively work to dismantle it in achieving these demands",
+      ],
+    },
+    {
+      category: "Arts, Culture, & History",
+      lead:
+        "DC has an amazing, rich culture of Blackness- through Gogo, murals, historical landmarks, restaurants, etc. Because of systemic racism and huge racial inequities in the city, DC’s Black residents and Black culture are at a greater risk of dying out. DC needs to be intentional about the legacy of our city and we need to unapologetically prioritize our Blackness.",
+      list: [
+        "DC Government will ensure all venues that support Black culture (music, art, food, care) will be prioritized and supported if in danger of closing",
+        "DC Government will be intentional about distributing funds to organizations and prioritize those that are Black led and serve Black people",
+        "As a form of recovery for the city, DC Government will invest in Black cultural institutions and Black cultural workers to envision and lead the process of rebuilding our city",
+      ],
+    },
+  ];
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./styles/_main.scss";
 import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/scrollspy";
+import StickyFill from "stickyfilljs";
+import $ from "jquery";
+
+const stickyElements = document.querySelectorAll(".sticky");
+StickyFill.add(stickyElements);
+
+$("#demand-nav a").click(() => {
+  console.log("here");
+  $("#demand-nav").collapse("hide");
+});
